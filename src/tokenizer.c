e03b2cc3d01b01d7f4fa417f20c847fb0a580128
student <sschubert@miners.utep.edu>
2020-09-21 22:55:42 -0600

    Added code to functions incomplete.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
 
+int space_char(char c) {
+  if(c == ' ' || c == '\t') {
+    return 1;
+  }
+  return 0;
+}
 
+int non_space_char(char c) {
+  if(c == ' ' || c == '\t') {
+    return 0;
+  }
+  return 1;
+}
 
+char word_start(char *str) {
+  for(;&str == '\0'; *str++;){
+    if(space_char(&str)){
+      return ++*str;
+    }
